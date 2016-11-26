b6c490ecc91ec91334771cf05d922641f869e4cb
zheng <490563168@qq.com>
2016-11-26 19:44:52 +0800

    no message

---
+++ demo.js
@@ -0,0 +1,58 @@
+
+
+
+var ap1 = new APlayer({
+    element: document.getElementById('player1'),
+    narrow: false,
+    autoplay: false,
+    showlrc: 3,
+    mutex: true,
+    theme: '#ad7a86',
+    mode: 'random',
+    listmaxheight: '80px',
+    music: [
+        {
+            title: 'あっちゅ～ま青春!',
+            author: '七森中☆ごらく部',
+            url: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.mp3',
+            pic: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.jpg',
+            lrc: 'music/あっちゅ～ま青春!.lrc'
+        },
+        {
+            title: 'secret base~君がくれたもの~',
+            author: '茅野愛衣',
+            url: 'http://devtest.qiniudn.com/secret base~.mp3',
+            pic: 'http://devtest.qiniudn.com/secret base~.jpg',
+            lrc: 'music/secret base~君がくれたもの~.lrc'
+        },
+        {
+            title: '回レ！雪月花',
+            author: '小倉唯',
+            url: 'http://devtest.qiniudn.com/回レ！雪月花.mp3',
+            pic: 'http://devtest.qiniudn.com/回レ！雪月花.jpg',
+            lrc: 'music/回レ！雪月花.lrc'
+        }
+    ]
+});
+	
+ap1.on('play', function () {
+    console.log('play');
+});
+ap1.on('play', function () {
+    console.log('play play');
+});
+ap1.on('pause', function () {
+    console.log('pause');
+});
+ap1.on('canplay', function () {
+    console.log('canplay');
+});
+ap1.on('playing', function () {
+    console.log('playing');
+});
+ap1.on('ended', function () {
+    console.log('ended');
+});
+ap1.on('error', function () {
+    console.log('error');
+});
